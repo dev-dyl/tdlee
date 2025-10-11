c239d4d8c3e14f8f153a4a99743fe8f22bb902d3
Dylan <dylanleeone@gmail.com>
2025-10-11 02:56:46 -0700

    Small fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,6 @@ export const metadata: Metadata = {
     shortcut: "/favicon.ico",
     apple: "/apple-touch-icon.png",
   },
-  themeColor: "#943c54",
 };
 
 export default function Root({ children }: { children: React.ReactNode }) {
