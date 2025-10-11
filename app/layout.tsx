5d6ee6050600f776728e2df07d7e1b64a0bf1b0b
Dylan <dylanleeone@gmail.com>
2025-10-11 02:54:17 -0700

    Add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,21 @@
 import "@/app/ui/global.css";
 
 import { josefin_sans } from "@/app/ui/fonts";
+import { Analytics } from '@vercel/analytics/next';
+
+// app/layout.tsx
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Taylor & Dylan",
+  icons: {
+    icon: "/favicon.ico",              // PNG/ICO fallback
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+  themeColor: "#943c54",
+};
 
 export default function Root({ children }: { children: React.ReactNode }) {
-  return <html lang="en" className="h-full"><body className={`${josefin_sans.className} antialiased h-full`}>{children}</body></html>;
+  return <html lang="en" className="h-full"><body className={`${josefin_sans.className} antialiased h-full`}>{children}<Analytics /></body></html>;
 }
